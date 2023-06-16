dff8aa1937aab2a717de0b38eacae79b2df937e2
stranik87 <tulkin.boynazarov@gmail.com>
2023-06-16 18:11:08 +0300

    100users

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ url = 'https://randomuser.me/api/'
 payload = {
     'results':100,
 }
-users = []
+
 response = requests.get(url=url,params=payload)
+
+db = TinyDB('users.json', indent=4)
+users = db.table('users')
+
 
 if response.status_code == 200:
     users_data = response.json()['results']
     for user in users_data:
-        users.append({
+        users.insert({
             'first_name':user['name']['first'],
             'last_name':user['name']['last'],
             'age':user['dob']['age'],
@@ -25,5 +29,3 @@ if response.status_code == 200:
             'nat':user['nat']
         })
         
-with open('users.json', 'w') as f:
-    json.dump(users,f,indent=4)
